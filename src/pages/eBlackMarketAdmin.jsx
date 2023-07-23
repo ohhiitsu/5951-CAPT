09a5a7717acf790f169d965401fc6ef748dd3d30
Fong Kai Jun <e0957867@u.nus.edu>
2023-07-23 23:40:32 +0800

    black market merge

--- src/pages/eBlackMarketAdmin.jsx
+++ src/pages/eBlackMarketAdmin.jsx
@@ -121,168 +121,168 @@ const EBlackMarketAdmin = () => {
       return product;
     });
 
-        setProducts(updatedProducts);
-        toast.success('Price updated!', { ...toastSettings });
-    };
+    setProducts(updatedProducts);
+    toast.success('Price updated!', { ...toastSettings });
+  };
 
-    const handleEditQuantity = async (productId, newQuantity) => {
-        if (!Number.isInteger(Number(newQuantity)) || newQuantity <= 0) {
-            alert("Please enter a valid quantity");
-            return;
-        }
+  const handleEditQuantity = async (productId, newQuantity) => {
+    if (!Number.isInteger(Number(newQuantity)) || newQuantity <= 0) {
+      alert("Please enter a valid quantity");
+      return;
+    }
 
-        try {
-            const productRef = doc(db, "eMarketDatabase", productId);
-            await updateDoc(productRef, { Quantity: Number(newQuantity) });
-            loadAllProducts();
-            toast.success('Quantity updated!', { ...toastSettings });
-        } catch (error) {
-            console.error(error);
-        }
-    };
+    try {
+      const productRef = doc(db, "eMarketDatabase", productId);
+      await updateDoc(productRef, { Quantity: Number(newQuantity) });
+      loadAllProducts();
+      toast.success('Quantity updated!', { ...toastSettings });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const handleDeleteProduct = async (productId) => {
-        try {
-            const productRef = doc(db, "eMarketDatabase", productId);
-            await deleteDoc(productRef);
-            loadAllProducts();
-            toast.success('Product deleted!', { ...toastSettings });
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  const handleDeleteProduct = async (productId) => {
+    try {
+      const productRef = doc(db, "eMarketDatabase", productId);
+      await deleteDoc(productRef);
+      loadAllProducts();
+      toast.success('Product deleted!', { ...toastSettings });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const toastSettings = {
-        position: "top-right",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-    };
+  const toastSettings = {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: false,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  };
 
-    if (adminEmails.includes(currentUserEmail)) {
-      return (
-        <div>
-          <button onClick={openUploadModal}>Upload a new product</button>
-          <Popup open={openUpload} onClose={closeUploadModal}>
-            <div>
-              <label>
-                Product Image:
-                <input type="file" accept="image/*" onChange={handleChange} />
-              </label>
-            </div>
-            <div>
-              <label>
-                Product Name:
-                <input
-                  type="text"
-                  placeholder="Name"
-                  onChange={(e) => setName(e.target.value)}
-                />
-              </label>
-            </div>
-            <div>
-              <label>
-                Product Quantity:
-                <input
-                  type="number"
-                  placeholder="Quantity"
-                  value={quantity}
-                  onChange={(e) => {
-                    const newQuantity = parseInt(e.target.value, 10);
-                    if (newQuantity >= 0) {
-                      setQuantity(newQuantity);
-                    }
-                  }}
-                />
-              </label>
-            </div>
-            <div>
-              <label>
-                Product Price ($):
-                <input
-                  type="number"
-                  placeholder="Price ($)"
-                  step="0.10"
-                  value={price}
-                  onChange={(e) => {
-                    const newPrice = parseFloat(e.target.value);
-                    if (!isNaN(newPrice) && newPrice >= 0) {
-                      const formattedPrice = newPrice.toFixed(2);
-                      setPrice(parseFloat(formattedPrice));
-                    }
-                  }}
-                />
-              </label>
-            </div>
-            <div>
-              <button onClick={handleUpload}>Save</button>
-            </div>
-            {uploaded && (
-              <p className="success-msg">New Product was uploaded successfully</p>
-            )}
-          </Popup>
-          <table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Price</th>
-                <th>Edit Price</th>
-                <th>Quantity</th>
-                <th>Edit Quantity</th>
-                <th>Delete Product</th>
-              </tr>
-            </thead>
-            <tbody>
-              {products.map((product) => (
-                <tr key={product.id}>
-                  <td>{product.name}</td>
-                  <td>${product.price.toFixed(2)}</td>
-                  <td>
-                    <button
-                      onClick={() =>
-                        handleEditPrice(product.id, prompt("Enter new price:"))
-                      }
-                    >
-                      <EditIcon />
-                    </button>{" "}
-                  </td>
-                  <td>{product.quantity}</td>
-                  <td>
-                    {" "}
-                    <button
-                      onClick={() =>
-                        handleEditQuantity(
-                          product.id,
-                          prompt("Enter new quantity:")
-                        )
-                      }
-                    >
-                      <EditIcon />
-                    </button>
-                  </td>
-                  <td>
-                    <button onClick={() => handleDeleteProduct(product.id)}>
-                      <DeleteIcon />
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          {loading && (
-            <div>
-              <p>Loading Products...</p>
-              <CircularProgress disableShrink />
-            </div>
+  if (adminEmails.includes(currentUserEmail)) {
+    return (
+      <div>
+        <button onClick={openUploadModal}>Upload a new product</button>
+        <Popup open={openUpload} onClose={closeUploadModal}>
+          <div>
+            <label>
+              Product Image:
+              <input type="file" accept="image/*" onChange={handleChange} />
+            </label>
+          </div>
+          <div>
+            <label>
+              Product Name:
+              <input
+                type="text"
+                placeholder="Name"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Product Quantity:
+              <input
+                type="number"
+                placeholder="Quantity"
+                value={quantity}
+                onChange={(e) => {
+                  const newQuantity = parseInt(e.target.value, 10);
+                  if (newQuantity >= 0) {
+                    setQuantity(newQuantity);
+                  }
+                }}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Product Price ($):
+              <input
+                type="number"
+                placeholder="Price ($)"
+                step="0.10"
+                value={price}
+                onChange={(e) => {
+                  const newPrice = parseFloat(e.target.value);
+                  if (!isNaN(newPrice) && newPrice >= 0) {
+                    const formattedPrice = newPrice.toFixed(2);
+                    setPrice(parseFloat(formattedPrice));
+                  }
+                }}
+              />
+            </label>
+          </div>
+          <div>
+            <button onClick={handleUpload}>Save</button>
+          </div>
+          {uploaded && (
+            <p className="success-msg">New Product was uploaded successfully</p>
           )}
-          <ToastContainer />
-        </div>
-      );
-    } else {
+        </Popup>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Edit Price</th>
+              <th>Quantity</th>
+              <th>Edit Quantity</th>
+              <th>Delete Product</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product.id}>
+                <td>{product.name}</td>
+                <td>${product.price.toFixed(2)}</td>
+                <td>
+                  <button
+                    onClick={() =>
+                      handleEditPrice(product.id, prompt("Enter new price:"))
+                    }
+                  >
+                    <EditIcon />
+                  </button>{" "}
+                </td>
+                <td>{product.quantity}</td>
+                <td>
+                  {" "}
+                  <button
+                    onClick={() =>
+                      handleEditQuantity(
+                        product.id,
+                        prompt("Enter new quantity:")
+                      )
+                    }
+                  >
+                    <EditIcon />
+                  </button>
+                </td>
+                <td>
+                  <button onClick={() => handleDeleteProduct(product.id)}>
+                    <DeleteIcon />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {loading && (
+          <div>
+            <p>Loading Products...</p>
+            <CircularProgress disableShrink />
+          </div>
+        )}
+        <ToastContainer />
+      </div>
+    );
+  } else {
     return <div>You have no authorisation.</div>;
   }
 };
